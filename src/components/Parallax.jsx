46609b67bd05c3dc1fcdc0dc1699a9c6154d8738
SadiLuis <sadiluisrueda.94@gmail.com>
2024-02-14 11:37:40 -0300

    Add new CV

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/prop-types */
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ParallaxBackground = ({ imageURL, children }) => {
   const controls = useAnimation();
   const [ref, inView] = useInView({
-    triggerOnce: false, // Cambia el valor a true si solo quieres que se active una vez
+    triggerOnce: false,
     threshold: 0.5,
   });
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
     if (inView) {
@@ -31,19 +32,32 @@ const ParallaxBackground = ({ imageURL, children }) => {
       transition: { duration: 1 },
     },
   };
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <motion.div
       ref={ref}
       animate={controls}
       initial="hidden"
       variants={containerVariants}
-      className=" fixed h-screen -z-10 flex items-center justify-center overflow-hidden"
+      className="fixed h-screen -z-10 flex items-center justify-center overflow-hidden"
     >
+      {imageLoaded ? null : (
+        <div className="absolute flex items-center justify-center w-full h-full bg-gray-200">
+          <div className="text-gray-600">Cargando...</div>
+        </div>
+      )}
       <motion.img
         src={imageURL}
         alt="Parallax Background"
-        className=" w-full h-full object-cover scale-150"
+        className={`w-full h-full object-cover scale-150 ${
+          imageLoaded ? "opacity-100" : "opacity-0"
+        }`}
         variants={imageVariants}
+        onLoad={handleImageLoad}
       />
       <div className="absolute">{children}</div>
     </motion.div>
